d579c05f8ac5c61f9ef89849c75818b51bdc121f
BilalChbanat <bilal.chbanat2003@gmail.com>
2025-02-04 15:44:25 +0100

    adding login component

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
 
+export class LoginComponent implements OnInit {
+  signupUsers: any[] = [];
+  signUpObjects: any = {
+    userName: '',
+    email: '',
+    address: '',
+    telephone: '',
+    DateOfBirth: '',
+    password: '',
+  };
+
+  loginObjects: any = {
+    email: '',
+    password: '',
+  };
+
+  loginFormSubmitted: boolean = false;
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  onSginUp() {
+    this.signUpObjects.push(this.signUpObjects);
+    localStorage.setItem('signupUsers', JSON.stringify(this.signupUsers));
+    this.signUpObjects = {
+      userName: '',
+      email: '',
+      address: '',
+      telephone: '',
+      DateOfBirth: '',
+      password: '',
+    };
+  }
 }
