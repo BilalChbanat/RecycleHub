18f18dc22fff427066d05cffcd090270907771b6
BilalChbanat <bilal.chbanat2003@gmail.com>
2025-02-04 15:43:19 +0100

    implementing the register component template

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,11 +1,54 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
-  imports: [],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
+  styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  formSubmitted: boolean = false;
+  signupUsers: any[] = [];
 
+  signUpObjects: any = {
+    userName: '',
+    email: '',
+    address: '',
+    telephone: '',
+    DateOfBirth: '',
+    password: '',
+  };
+
+  ngOnInit() {
+    const storedUsers = localStorage.getItem('signupUsers');
+    if (storedUsers) {
+      this.signupUsers = JSON.parse(storedUsers);
+    }
+  }
+
+  onSignUp() {
+    this.formSubmitted = true;
+
+    if (this.signUpObjects.userName && this.signUpObjects.email && this.signUpObjects.address &&
+      this.signUpObjects.telephone && this.signUpObjects.DateOfBirth && this.signUpObjects.password) {
+
+      this.signupUsers.push({...this.signUpObjects});
+      localStorage.setItem('signupUsers', JSON.stringify(this.signupUsers));
+
+      this.signUpObjects = {
+        userName: '',
+        email: '',
+        address: '',
+        telephone: '',
+        DateOfBirth: '',
+        password: '',
+      };
+      this.formSubmitted = false;
+    }
+  }
 }
