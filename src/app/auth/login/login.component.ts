dab44e869ad4a56ff3c7df158b9f1e55fadccbae
BilalChbanat <bilal.chbanat2003@gmail.com>
2025-02-06 09:36:23 +0100

    fixing the login errors

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,29 +1,16 @@
-import {Component, OnInit} from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import {FormsModule} from '@angular/forms';
-import {CommonModule} from '@angular/common';
-import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  standalone: true,
-  imports: [
-    FormsModule,
-    CommonModule,
-  ],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
+  imports: [
+    FormsModule
+  ]
 })
-export class LoginComponent implements OnInit {
-  signupUsers: any[] = [];
-  signUpObjects: any = {
-    userName: '',
-    email: '',
-    address: '',
-    telephone: '',
-    DateOfBirth: '',
-    password: '',
-  };
-
+export class LoginComponent {
   loginObjects: any = {
     email: '',
     password: '',
@@ -31,50 +18,21 @@ export class LoginComponent implements OnInit {
 
   loginFormSubmitted: boolean = false;
 
-  constructor(private router: Router) {
-  }
-
-  ngOnInit() {
-    const storedUsers = localStorage.getItem('signupUsers');
-    if (storedUsers) {
-      this.signupUsers = JSON.parse(storedUsers);
-    }
-  }
-
-  onSignUp() {
-    this.signupUsers.push({...this.signUpObjects});
-
-    localStorage.setItem('signupUsers', JSON.stringify(this.signupUsers));
-
-    this.signUpObjects = {
-      userName: '',
-      email: '',
-      address: '',
-      telephone: '',
-      DateOfBirth: '',
-      password: '',
-    };
-
-    alert('Registration successful!');
-  }
+  constructor(private router: Router) {}
 
   onLogin() {
     this.loginFormSubmitted = true;
 
-    const isUserExist = this.signupUsers.find(
-      (user) =>
-        user.email === this.loginObjects.email &&
-        user.password === this.loginObjects.password
-    );
-
-    if (isUserExist) {
-      const currentUser = isUserExist.loginObjects;
-      console.log('currentUser', currentUser);
-        localStorage.setItem('isUserLoggedIn', 'true');
+    if (this.loginObjects.email && this.loginObjects.password) {
+      console.log('Login successful', this.loginObjects);
 
       this.router.navigate(['/dashboard']);
     } else {
-      alert('Invalid email or password');
+      console.log('Please fill in all fields');
     }
+  }
+
+  onSignUp() {
+    this.router.navigate(['/register']);
   }
 }
